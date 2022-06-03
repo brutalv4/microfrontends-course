5eeee14e921f8a4ac0cf25cb87ac5090f2b09ca5
Vladimir Bredikhin <brutal.v4@gmail.com>
2022-06-03 20:49:36 +0300

    Add mount fn's for products/cart

--- ecomm/cart/src/bootstrap.js
+++ ecomm/cart/src/bootstrap.js
@@ -2,6 +2,19 @@ import faker from "faker";
 
 console.log("Cart");
 
-const cartText = `<h4>You have ${faker.random.number()} items in your cart</h4>`;
+const DEV_SELECTOR = "#dev-cart";
 
-document.querySelector("#dev-cart").innerHTML = cartText;
+const mount = (el) => {
+  const cartText = `<h4>You have ${faker.random.number()} items in your cart</h4>`;
+  el.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector(DEV_SELECTOR);
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -2,13 +2,27 @@ import faker from "faker";
 
 console.log("Products");
 
-let products = "<ol>";
+const DEV_SELECTOR = "#dev-products";
 
-for (let i = 0; i < 3; i++) {
-  const name = faker.commerce.productName();
-  products += `<li>${name}</li>`;
+const mount = (el) => {
+  let products = "<ol>";
+
+  for (let i = 0; i < 3; i++) {
+    const name = faker.commerce.productName();
+    products += `<li>${name}</li>`;
+  }
+
+  products += "</ol>";
+
+  el.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector(DEV_SELECTOR);
+
+  if (el) {
+    mount(el);
+  }
 }
 
-products += "</ol>";
-
-document.querySelector("#dev-products").innerHTML = products;
+export { mount };
